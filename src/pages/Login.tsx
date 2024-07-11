b52f6561b1f453c1273b8f3b9557a12cac697909
nargis21 <nargisakther1933@gmail.com>
2024-07-11 19:49:08 +0600

    login redirect done

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,14 +1,11 @@
 import { Button } from "antd";
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
 import { verifyToken } from "../utils/verifyToken";
 import { useAppDispatch } from "../redux/hook";
-import { setUser } from "../redux/features/auth/authSlice";
-
-type TFormData = {
-    userId: string,
-    password: string
-}
+import { setUser, TUser } from "../redux/features/auth/authSlice";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 const Login = () => {
     const { register, handleSubmit } = useForm({
@@ -19,20 +16,27 @@ const Login = () => {
     })
 
     const dispatch = useAppDispatch()
-
+    const navigate = useNavigate()
     const [login, { data, error }] = useLoginMutation()
     console.log('data =>', data);
     console.log('error =>', error);
 
-    const onSubmit = async (data: TFormData) => {
-        const userInfo = {
-            id: data.userId,
-            password: data.password
+    const onSubmit = async (data: FieldValues) => {
+        const toastId = toast.loading('Logging In')
+        try {
+            const userInfo = {
+                id: data.userId,
+                password: data.password
+            }
+            const res = await login(userInfo).unwrap()
+            const user = verifyToken(res.data.accessToken) as TUser
+            dispatch(setUser({ user: user, token: res.data.accessToken }))
+            toast.success("Logged in", { id: toastId, duration: 2000 })
+            navigate(`/${user.role}/dashboard`)
         }
-        const res = await login(userInfo).unwrap()
-        const user = verifyToken(res.data.accessToken)
-
-        dispatch(setUser({ user: user, token: res.data.accessToken }))
+        catch (err) {
+            toast.error('Something Went Wrong', { id: toastId, duration: 2000 })
+        }
 
     }
 
