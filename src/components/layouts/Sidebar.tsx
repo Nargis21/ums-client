87749c0e4aba903688052b64e4d624c1cef1ad34
nargis21 <nargisakther1933@gmail.com>
2024-07-11 14:49:15 +0600

    role based sidebar items added

--- src/components/layouts/Sidebar.tsx
+++ src/components/layouts/Sidebar.tsx
@@ -1,9 +1,36 @@
 import { Layout, Menu } from 'antd';
 import { adminPaths } from '../../routes/adminRoutes';
 import { sidebarItemsGenerator } from '../../utils/sidebarItemsGenerator';
+import { facultyPaths } from '../../routes/facultyRoutes';
+import { studentPaths } from '../../routes/studentRoutes';
 const { Sider } = Layout;
 
+const userRole = {
+    ADMIN: 'admin',
+    FACULTY: 'faculty',
+    STUDENT: 'student'
+}
+
 const Sidebar = () => {
+    const role = 'student'
+    let sidebarItems;
+
+    switch (role) {
+        case userRole.ADMIN:
+            sidebarItems = sidebarItemsGenerator(adminPaths, userRole.ADMIN)
+            break;
+        case userRole.FACULTY:
+            sidebarItems = sidebarItemsGenerator(facultyPaths, userRole.FACULTY)
+            break;
+        case userRole.STUDENT:
+            sidebarItems = sidebarItemsGenerator(studentPaths, userRole.STUDENT)
+            break;
+
+        default:
+            break;
+    }
+
+
     return (
         <Sider
             breakpoint="lg"
@@ -12,7 +39,7 @@ const Sidebar = () => {
             <div style={{ color: "white", height: '4rem', display: 'flex', justifyContent: "center", alignItems: "center" }}>
                 <h1>PH University</h1>
             </div>
-            <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={sidebarItemsGenerator(adminPaths, 'admin')} />
+            <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={sidebarItems} />
         </Sider>
     );
 };
