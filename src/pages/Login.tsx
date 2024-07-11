df25350785d15d52a502fe2e73f8b6d9ab390f6d
nargis21 <nargisakther1933@gmail.com>
2024-07-11 17:39:31 +0600

    login with token done

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,48 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+import { verifyToken } from "../utils/verifyToken";
+import { useAppDispatch } from "../redux/hook";
+import { setUser } from "../redux/features/auth/authSlice";
 
 const Login = () => {
+    const { register, handleSubmit } = useForm({
+        defaultValues: {
+            userId: "A-0001",
+            password: "admin123"
+        }
+    })
+
+    const dispatch = useAppDispatch()
+
+    const [login, { data, error }] = useLoginMutation()
+    console.log('data =>', data);
+    console.log('error =>', error);
+
+    const onSubmit = async (data) => {
+        const userInfo = {
+            id: data.userId,
+            password: data.password
+        }
+        const res = await login(userInfo).unwrap()
+        const user = verifyToken(res.data.accessToken)
+
+        dispatch(setUser({ user: user, token: res.data.accessToken }))
+
+    }
+
     return (
-        <div>
-            Login
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+                <label htmlFor="id">ID</label>
+                <input type="text" id="id" {...register('userId')} />
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input type="text" id="password" {...register('password')} />
+            </div>
+            <Button htmlType="submit">Login</Button>
+        </form>
     );
 };
 
