b825abfb590837a19f0c7ad04b94dfab48cbf9cb
nargis21 <nargisakther1933@gmail.com>
2024-07-13 11:03:05 +0600

    user not found handling added

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -8,6 +8,7 @@ import {
 } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../store";
 import { logout, setUser } from "../features/auth/authSlice";
+import { toast } from "sonner";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:5000/api/v1",
@@ -27,6 +28,11 @@ const baseQueryWithRefreshToken: BaseQueryFn<
   DefinitionType
 > = async (args, api, extraOptions): Promise<any> => {
   let result = await baseQuery(args, api, extraOptions);
+
+  if (result.error?.status === 404) {
+    toast.error("User not found", { duration: 2000 });
+  }
+
   if (result.error?.status === 401) {
     const res = await fetch("http://localhost:5000/api/v1/auth/refresh-token", {
       method: "POST",
