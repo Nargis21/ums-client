1e189e8873b9ce1276a5cd53a4ca8cd7b6cd4b54
nargis21 <nargisakther1933@gmail.com>
2024-07-13 13:04:41 +0600

    academic semester year added

--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -3,18 +3,25 @@ import PHForm from "../../../components/form/PHForm";
 import { FieldValues, SubmitHandler } from "react-hook-form";
 import PHSelect from "../../../components/form/PHSelect";
 
-const options = [
+const nameOptions = [
     { value: '01', label: 'Autumn' },
     { value: '02', label: 'Summer' },
     { value: '03', label: 'Fall' },
 ]
 
+const getCurrentYear = new Date().getFullYear()
+const yearOptions = [0, 1, 2, 3, 4].map((number) => ({
+    value: String(getCurrentYear + number),
+    label: String(getCurrentYear + number)
+}))
+
 const CreateAcademicSemester = () => {
     const onSubmit: SubmitHandler<FieldValues> = (data) => {
-        const name = options[Number(data.name) - 1].label
+        const name = nameOptions[Number(data.name) - 1].label
         const semesterData = {
             name,
-            code: data.name
+            code: data.name,
+            year: data.year
         }
         console.log(semesterData);
     }
@@ -23,7 +30,10 @@ const CreateAcademicSemester = () => {
         <Flex justify="center" align="center">
             <Col span={6}>
                 <PHForm onSubmit={onSubmit}>
-                    <PHSelect label="Name" name="name" options={options} />
+                    <PHSelect label="Name" name="name" options={nameOptions} />
+                    <PHSelect label="Year" name="year" options={yearOptions} />
+                    <PHSelect label="Start Month" name="startMonth" options={nameOptions} />
+                    <PHSelect label="End Month" name="endMonth" options={nameOptions} />
                     <Button htmlType="submit" >Submit</Button>
                 </PHForm>
             </Col>
