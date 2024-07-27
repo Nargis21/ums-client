77c06228e7a6beb72bc08b01600abb2c2c0436d9
nargis21 <nargisakther1933@gmail.com>
2024-07-27 10:30:18 +0600

    add student api added

---
+++ src/redux/features/admin/userManagement.ts
@@ -0,0 +1,37 @@
+import { baseApi } from "../../api/baseApi";
+
+const userManagementApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    // getAllSemesters: builder.query({
+    //   query: (args) => {
+    //     const params = new URLSearchParams();
+    //     if (args) {
+    //       args.forEach((item: TQueryParams) => {
+    //         params.append(item.name, item.value as string);
+    //       });
+    //     }
+
+    //     return {
+    //       url: "/academic-semesters",
+    //       method: "GET",
+    //       params: params,
+    //     };
+    //   },
+    //   transformResponse: (response: TResponseRedux<TAcademicSemester[]>) => {
+    //     return {
+    //       data: response.data,
+    //       meta: response.meta,
+    //     };
+    //   },
+    // }),
+    addStudent: builder.mutation({
+      query: (data) => ({
+        url: "/users/create-student",
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+export const { useAddStudentMutation } = userManagementApi;
