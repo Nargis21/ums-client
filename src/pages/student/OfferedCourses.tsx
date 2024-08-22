e305f968c92a95e30b92fea37f7f554d9ea2adce
nargis21 <nargisakther1933@gmail.com>
2024-08-23 02:17:01 +0600

    show my offered course done

--- src/pages/student/OfferedCourses.tsx
+++ src/pages/student/OfferedCourses.tsx
@@ -1,21 +1,58 @@
+import { Button, Col, Row } from "antd";
 import { useGetAllStudentOfferedCoursesQuery } from "../../redux/features/student/studentCourseManagement";
+
+type TCourse = {
+    [index: string]: any
+}
 
 const OfferedCourses = () => {
     const { data: offeredCourseData } = useGetAllStudentOfferedCoursesQuery(undefined)
 
-    const singleObject = offeredCourseData?.data?.reduce((acc, item) => {
+    const singleObject = offeredCourseData?.data?.reduce((acc: TCourse, item) => {
         const key = item.course.title
         acc[key] = acc[key] || { courseTitle: key, sections: [] };
         acc[key].sections.push({ section: item.section, _id: item._id })
         return acc
     }, {})
 
-    console.log(Object.values(singleObject ? singleObject : {}));
+    const modifiedData = Object.values(singleObject ? singleObject : {});
 
     return (
-        <div>
-            Offered Course
-        </div>
+        <Row gutter={[0, 20]}>
+            {modifiedData?.map((item) => {
+                return (
+                    <Col span={24} style={{ border: 'solid #d4d4d4 2px' }}>
+                        <div style={{ padding: '10px' }}>
+                            <h2>{item.courseTitle}</h2>
+                        </div>
+                        <div>
+                            {item?.sections?.map((section) => {
+                                return (
+                                    <Row
+                                        justify="space-between"
+                                        align="middle"
+                                        style={{ borderTop: 'solid #d4d4d4 2px', padding: '10px' }}
+                                    >
+                                        <Col span={5}>Section: {section.section} </Col>
+                                        <Col span={5}>
+                                            days:{' '}
+                                            {section?.days?.map((day: string) => (
+                                                <span> {day} </span>
+                                            ))}
+                                        </Col>
+                                        <Col span={5}>Start Time: {section.startTime} </Col>
+                                        <Col span={5}>End Time: {section.endTime} </Col>
+                                        <Button>
+                                            Enroll
+                                        </Button>
+                                    </Row>
+                                );
+                            })}
+                        </div>
+                    </Col>
+                );
+            })}
+        </Row>
     );
 };
 
