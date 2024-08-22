f02f8213e0d01e15d6afdb60bc119b5c501ff396
nargis21 <nargisakther1933@gmail.com>
2024-08-23 01:59:10 +0600

    simplify student offered course data

--- src/pages/student/OfferedCourses.tsx
+++ src/pages/student/OfferedCourses.tsx
@@ -1,8 +1,17 @@
 import { useGetAllStudentOfferedCoursesQuery } from "../../redux/features/student/studentCourseManagement";
 
 const OfferedCourses = () => {
-    const { data } = useGetAllStudentOfferedCoursesQuery(undefined)
-    console.log(data);
+    const { data: offeredCourseData } = useGetAllStudentOfferedCoursesQuery(undefined)
+
+    const singleObject = offeredCourseData?.data?.reduce((acc, item) => {
+        const key = item.course.title
+        acc[key] = acc[key] || { courseTitle: key, sections: [] };
+        acc[key].sections.push({ section: item.section, _id: item._id })
+        return acc
+    }, {})
+
+    console.log(Object.values(singleObject ? singleObject : {}));
+
     return (
         <div>
             Offered Course
@@ -11,3 +20,24 @@ const OfferedCourses = () => {
 };
 
 export default OfferedCourses;
+
+
+// [
+//   { course: { title: 'React' }, section: 1, _id: '45345' },
+//   { course: { title: 'React' }, section: 2, _id: '45345' },
+//   { course: { title: 'Redux' }, section: 1, _id: '45345' },
+// ];
+
+// [
+//   {
+//     courseTitle: 'React',
+//     sections: [
+//       { section: 1, _id: 'ADFa4345' },
+//       { section: 2, _id: 'ADFa4345' },
+//     ],
+//   },
+//   {
+//     courseTitle: 'Redux',
+//     sections: [{ section: 1, _id: 'ADFa4345' }],
+//   },
+// ];
